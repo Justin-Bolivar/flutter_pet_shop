584514523ba3b5d48b76adb3e1fe3fbc6bb7db8e
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2024-02-22 15:48:24 +0800

    updated routes

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pet_shop/screens/details_screen.dart';
+import 'package:provider/provider.dart';
+import 'providers/cart_provider.dart';
+import 'screens/cart.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: "/products",
+      routes: {
+        "/products": (context) => const ProductsPage(),
+        "/details": (context) =>
+            PetDetailScreen(), // replace somePetObject with the actual pet object
+        "/cart": (context) => const CartPage(),
+      },
+    );
+  }
+}
