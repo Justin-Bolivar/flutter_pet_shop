c495672a80e193c7ad1d7c72fda4f86cea8bc916
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2024-02-22 17:53:21 +0800

    added the cart on deails page

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pet_shop/const.dart';
+import 'package:flutter_pet_shop/providers/cart_provider.dart';
+import 'package:provider/provider.dart';
 
 class DetailsPage extends StatelessWidget {
   const DetailsPage({super.key, required this.pets});
@@ -8,29 +10,75 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(pets.name),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Details of ${pets.name}',
-              style: const TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Price: \$${pets.price}',
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Description: ${pets.store}',
-              style: const TextStyle(fontSize: 18),
+    return ChangeNotifierProvider.value(
+      value: Provider.of<CartProvider>(context),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(pets.name),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/cart");
+              },
+              icon: const Icon(
+                Icons.shopping_cart,
+                color: Colors.black,
+              ),
             ),
           ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    'assets/${pets.image}',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  '${pets.name}',
+                  style: const TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  '\$${pets.price}',
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  '${pets.store}',
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    final cartProvider =
+                        Provider.of<CartProvider>(context, listen: false);
+                    cartProvider.add(pets);
+                  },
+                  child: const Text('Add to Cart'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
